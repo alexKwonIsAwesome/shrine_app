42628625db1715d42a24543641609d7be923d748
alexKwonIsAwesome <khwalex@gmail.com>
2019-09-21 09:26:45 +0900

    Add appbar ui with title and icons

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,36 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {},
+        ),
+        title: Text('SHRINE'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {
+              print('Filter button');
+            },
+          )
+        ],
+      ),
       body: Center(
         child: Text('you did it!'),
       ),
