ed2d51d0a3eb6dee63df92b69cb9a497080e2c5d
alexKwonIsAwesome <khwalex@gmail.com>
2019-09-21 22:01:51 +0900

    Modify home page to render products data
    - Home page now renders cards with product data.

--- lib/home.dart
+++ lib/home.dart
@@ -1,34 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/product.dart';
+import 'model/product_repository.dart';
 
 class Home extends StatelessWidget {
-  List<Card> _buildGridCards(int count) {
-    List<Card> cards = List.generate(count, (int index) {
-      return (Card(
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
         clipBehavior: Clip.antiAlias,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             AspectRatio(
-              aspectRatio: 18.0 / 11.0,
-              child: Image.asset('assets/diamond.png'),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Title'),
-                  SizedBox(height: 8.0),
-                  Text('Secondary Text')
-                ],
+                aspectRatio: 18.0 / 11.0,
+                child: Image.asset(
+                  product.assetName,
+                  package: product.assetPackage,
+                  fit: BoxFit.fitWidth,
+                )),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 20.0, 16.0, 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      product.name,
+                      style: theme.textTheme.title,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.body2,
+                    )
+                  ],
+                ),
               ),
             )
           ],
         ),
-      ));
-    });
-
-    return cards;
+      );
+    }).toList();
   }
 
   @override
@@ -68,7 +88,7 @@ class Home extends StatelessWidget {
           crossAxisCount: 2,
           padding: EdgeInsets.all(16.0),
           childAspectRatio: 0.88 / 1.0,
-          children: _buildGridCards(10)),
+          children: _buildGridCards(context)),
       resizeToAvoidBottomInset: false,
     );
   }
