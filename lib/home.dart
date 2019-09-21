ddc75f1518bcd6da36b079e14f2b8a08d0d24631
alexKwonIsAwesome <khwalex@gmail.com>
2019-09-21 10:23:22 +0900

    Modify home to build list of cards

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(count, (int index) {
+      return (Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset('assets/diamond.png'),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Title'),
+                  SizedBox(height: 8.0),
+                  Text('Secondary Text')
+                ],
+              ),
+            )
+          ],
+        ),
+      ));
+    });
+
+    return cards;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,9 +64,11 @@ class Home extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Text('you did it!'),
-      ),
+      body: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(16.0),
+          childAspectRatio: 0.88 / 1.0,
+          children: _buildGridCards(10)),
       resizeToAvoidBottomInset: false,
     );
   }
